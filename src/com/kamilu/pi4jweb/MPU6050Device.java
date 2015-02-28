14588157441eed5e1ae48063efa6960b03897f6f
Ja <Ja@Kamil>
2015-03-01 00:07:39 +0100

    mpu fixed

--- src/com/kamilu/pi4jweb/MPU6050Device.java
+++ src/com/kamilu/pi4jweb/MPU6050Device.java
@@ -3,7 +3,6 @@ package com.kamilu.pi4jweb;
 import java.io.IOException;
 import java.io.Serializable;
 
-import com.alsnightsoft.vaadin.widgets.canvasplus.CanvasPlus;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
@@ -19,14 +18,12 @@ public class MPU6050Device implements Serializable {
 	float gyroXangle;
 	float gyroYangle;
 	float gyroZangle;
-	double CFangleX;
-	double CFangleY;
+	double cfAngleX;
+	double cfAngleY;
 	long startTime;
-	private CanvasPlus canvas;
 	private Label label;
 
-	public MPU6050Device(CanvasPlus canvas, Label label) {
-		this.canvas = canvas;
+	public MPU6050Device(Label label) {
 		this.label = label;
 
 	}
@@ -45,80 +42,82 @@ public class MPU6050Device implements Serializable {
 		}
 	}
 
-	public void mockStartReading() {
-		new MockSensorsThread().start();
-	}
-
 	class SensorsThread extends Thread {
 		@Override
 		public void run() {
-			try {
-				while (true) {
-					accelData = new byte[6];
-					gyroData = new byte[6];
-					device.read(0x3B, accelData, 0, 6);
-					int aX = (accelData[0] << 8) + accelData[1];
-					if (aX >= 32768) {
-						aX = -((65535 - aX) + 1);
-					}
-					int aY = (accelData[2] << 8) + accelData[3];
-					if (aY >= 32768) {
-						aY = -((65535 - aY) + 1);
-					}
-					int aZ = (accelData[4] << 8) + accelData[5];
-					if (aZ >= 32768) {
-						aZ = -((65535 - aZ) + 1);
-					}
-					double aXsc = aX / 16384.0;
-					double aYsc = aY / 16384.0;
-					double aZsc = aZ / 16384.0;
-					device.read(0x43, gyroData, 0, 6);
-					int gX = (gyroData[0] << 8) + gyroData[1];
-					if (gX >= 0x8000) {
-						gX = -((65535 - gX) + 1);
-					}
-					int gY = (gyroData[2] << 8) + gyroData[3];
-					if (gY >= 0x8000) {
-						gY = -((65535 - gY) + 1);
-					}
-					int gZ = (gyroData[4] << 8) + gyroData[5];
-					if (gZ >= 0x8000) {
-						gZ = -((65535 - gZ) + 1);
-					}
-					final float gain = 0.07f, DT = 0.002f, alfa = 0.8f;
-					float rate_gyr_x = (float) gX * gain;
-					float rate_gyr_y = (float) gY * gain;
-					float rate_gyr_z = (float) gZ * gain;
-					gyroXangle += rate_gyr_x * DT;
-					gyroYangle += rate_gyr_y * DT;
-					gyroZangle += rate_gyr_z * DT;
-					if (aXsc > 180) {
-						aXsc -= (float) 360.0;
-					}
-					if (aYsc > 180) {
-						aYsc -= (float) 360.0;
-					}
-					double rotX = Math.toDegrees(Math.atan2(aXsc, Math.sqrt((aYsc * aYsc) + (aZsc * aZsc))));
-					double rotY = -Math.toDegrees(Math.atan2(aYsc, Math.sqrt((aXsc * aXsc) + (aZsc * aZsc))));
-					CFangleX = alfa * (CFangleX + rate_gyr_x * DT) + (1 - alfa) * rotX;
-					CFangleY = alfa * (CFangleY + rate_gyr_x * DT) + (1 - alfa) * rotY;
-					label.setValue("CFangleX: " + CFangleX + ", CFangleY: " + CFangleY);
+			while (true) {
+				try {
 					Thread.sleep(1000);
+					UI.getCurrent().access(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								accelData = new byte[6];
+								gyroData = new byte[6];
+								device.read(0x3B, accelData, 0, 6);
+								int aX = (accelData[0] << 8) + accelData[1];
+								if (aX >= 32768) {
+									aX = -((65535 - aX) + 1);
+								}
+								int aY = (accelData[2] << 8) + accelData[3];
+								if (aY >= 32768) {
+									aY = -((65535 - aY) + 1);
+								}
+								int aZ = (accelData[4] << 8) + accelData[5];
+								if (aZ >= 32768) {
+									aZ = -((65535 - aZ) + 1);
+								}
+								double aXsc = aX / 16384.0;
+								double aYsc = aY / 16384.0;
+								double aZsc = aZ / 16384.0;
+								device.read(0x43, gyroData, 0, 6);
+								int gX = (gyroData[0] << 8) + gyroData[1];
+								if (gX >= 0x8000) {
+									gX = -((65535 - gX) + 1);
+								}
+								int gY = (gyroData[2] << 8) + gyroData[3];
+								if (gY >= 0x8000) {
+									gY = -((65535 - gY) + 1);
+								}
+								int gZ = (gyroData[4] << 8) + gyroData[5];
+								if (gZ >= 0x8000) {
+									gZ = -((65535 - gZ) + 1);
+								}
+								final float gain = 0.07f, DT = 0.002f, alfa = 0.8f;
+								float rate_gyr_x = (float) gX * gain;
+								float rate_gyr_y = (float) gY * gain;
+								float rate_gyr_z = (float) gZ * gain;
+								gyroXangle += rate_gyr_x * DT;
+								gyroYangle += rate_gyr_y * DT;
+								gyroZangle += rate_gyr_z * DT;
+								if (aXsc > 180) {
+									aXsc -= (float) 360.0;
+								}
+								if (aYsc > 180) {
+									aYsc -= (float) 360.0;
+								}
+								double rotX = Math.toDegrees(Math.atan2(aXsc, Math.sqrt((aYsc * aYsc) + (aZsc * aZsc))));
+								double rotY = -Math.toDegrees(Math.atan2(aYsc, Math.sqrt((aXsc * aXsc) + (aZsc * aZsc))));
+								cfAngleX = alfa * (cfAngleX + rate_gyr_x * DT) + (1 - alfa) * rotX;
+								cfAngleY = alfa * (cfAngleY + rate_gyr_x * DT) + (1 - alfa) * rotY;
+								label.setValue("CFangleX: " + cfAngleX + ", CFangleY: " + cfAngleY);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-			} catch (InterruptedException | IOException e) {
-				e.printStackTrace();
 			}
 		}
 	}
 
 	class MockSensorsThread extends Thread {
-		int count = 0;
-
 		@Override
 		public void run() {
 			try {
-				// Update the data for a while
-				while (count < 100) {
+				while (true) {
 					Thread.sleep(2000);
 					UI.getCurrent().access(new Runnable() {
 						@Override
@@ -127,28 +126,18 @@ public class MPU6050Device implements Serializable {
 						}
 					});
 				}
-				UI.getCurrent().access(new Runnable() {
-					@Override
-					public void run() {
-						label.setValue("Done");
-					}
-				});
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public void stop() {
+	public void stopI2cBus() {
 		try {
 			bus.close();
+			label.setValue("Device stopped");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public void mockStop() {
-		label.setValue("Device stopped");
-	}
-
 }
--- src/com/kamilu/pi4jweb/VaadinMPU6050Presenter.java
+++ src/com/kamilu/pi4jweb/VaadinMPU6050Presenter.java
@@ -1,29 +1,23 @@
 package com.kamilu.pi4jweb;
 
-import com.alsnightsoft.vaadin.widgets.canvasplus.CanvasPlus;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class VaadinMPU6050Presenter extends CustomComponent {
 
 	private static final long serialVersionUID = 1L;
 	private VerticalLayout rootLayout;
-	private CanvasPlus canvas;
 	private MPU6050Device device;
 	private Label label;
 	private ToggleButton power;
 
 	public VaadinMPU6050Presenter() {
-		canvas = new CanvasPlus();
-		canvas.setHeight("100px");
-		canvas.setWidth("100px");
 		label = new Label("Test");
-		canvas.setVisible(false);
 		label.setVisible(false);
+		label.setImmediate(true);
 		power = new ToggleButton(false, "");
 		power.addClickListener(new Button.ClickListener() {
 
@@ -33,18 +27,16 @@ public class VaadinMPU6050Presenter extends CustomComponent {
 			public void buttonClick(ClickEvent event) {
 
 				if (!power.getValue()) {
-					canvas.setVisible(true);
 					label.setVisible(true);
 					device.startReading();
 				} else {
-					canvas.setVisible(false);
 					label.setVisible(false);
-					device.stop();
+					device.stopI2cBus();
 				}
 				power.toggle();
 			}
 		});
-		device = new MPU6050Device(canvas, label);
+		device = new MPU6050Device(label);
 		rootLayout = new VerticalLayout(power, label);
 		setCompositionRoot(rootLayout);
 	}
